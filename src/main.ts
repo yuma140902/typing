4ff2ec5e9b29ab3d8b15c56fb9cf55adeb519010
yuma14 <yuma140902@gmail.com>
2024-05-31 19:58:22 +0900

    feat: app name

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,7 @@ import { setupCounter } from './counter.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <h1>Typing</h1>
   </div>
 `;
 
