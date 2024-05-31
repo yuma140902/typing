bf050dda3d8fc4eec4c330d957d280b06c5337d4
yuma14 <yuma140902@gmail.com>
2024-06-01 04:00:21 +0900

    feat: update GameObject

--- src/game_object.ts
+++ src/game_object.ts
@@ -1,24 +1,23 @@
 import { type EasingValue } from './easing';
 import { type Theme } from './theme';
 
-export type GameObject = Rectangle | TitleCharacter | TextBlock;
+export type GameObject = Rectangle | TextBlock;
 
 export type Rectangle = {
   tag: 'rectangle';
   position: EasingValue<{ x: number; y: number }>;
-  color: keyof Theme;
-  width: number;
-  height: number;
-};
-
-export type TitleCharacter = {
-  tag: 'title-character';
-  position: { x: number; y: number };
-  character: string;
+  strokeColor?: keyof Theme;
+  fillColor?: keyof Theme;
+  width: EasingValue<number>;
+  height: EasingValue<number>;
 };
 
 export type TextBlock = {
   tag: 'text-block';
-  position: { x: number; y: number };
+  position: EasingValue<{ x: number; y: number }>;
+  strokeColor?: keyof Theme;
+  fillColor?: keyof Theme;
+  align?: CanvasTextAlign;
+  font?: string;
   text: string;
 };
--- src/render.ts
+++ src/render.ts
@@ -1,11 +1,11 @@
 import { getFpsCounter } from './debug_fps';
-import { type Easing, animatePoint } from './easing';
 import {
-  type Rectangle,
-  type GameObject,
-  type TitleCharacter,
-  type TextBlock,
-} from './game_object';
+  type Easing,
+  animatePoint,
+  EasingValue,
+  animateNumber,
+} from './easing';
+import { type Rectangle, type GameObject, type TextBlock } from './game_object';
 import { type GameState } from './game_state';
 import { type Theme } from './theme';
 import {
@@ -52,6 +52,14 @@ const buildLetterTimeline = (
     start,
     duration,
   });
+  const fixed = <T>(value: T): EasingValue<T> => {
+    return {
+      from: value,
+      to: value,
+      easing: { type: { tag: 'fixed' }, start, duration: ms(0) },
+    };
+  };
+  const font = '48px IBMPlexSans, IBMPlexSansJP';
 
   for (let i = 0; i < letters.length; i++) {
     const width = ctx.measureText(s).width;
@@ -60,9 +68,9 @@ const buildLetterTimeline = (
 
     const rect: Rectangle = {
       tag: 'rectangle',
-      width: 4,
-      height: 60,
-      color: 'primary',
+      width: fixed(4),
+      height: fixed(60),
+      fillColor: 'primary',
       position: {
         from: prevPos,
         to: nextPos,
@@ -75,10 +83,12 @@ const buildLetterTimeline = (
       obj: rect,
     });
 
-    const chara: TitleCharacter = {
-      tag: 'title-character',
-      position: { x: nextX, y },
-      character: letters[i],
+    const chara: TextBlock = {
+      tag: 'text-block',
+      fillColor: 'foreground',
+      font,
+      position: fixed({ x: nextX, y: y }),
+      text: letters[i],
     };
     elements.push({
       appearAt: timeAfter(start, ms(i * delay + delay * (1 + 0.75 / 2))),
@@ -91,11 +101,11 @@ const buildLetterTimeline = (
   }
 
   const widthLast = ctx.measureText(s).width;
-  const rect: Rectangle = {
+  const lastCursor: Rectangle = {
     tag: 'rectangle',
-    width: 4,
-    height: 60,
-    color: 'primary',
+    width: fixed(4),
+    height: fixed(60),
+    fillColor: 'primary',
     position: {
       from: prevPos,
       to: {
@@ -107,27 +117,106 @@ const buildLetterTimeline = (
   };
   elements.push({
     appearAt: timeAfter(start, ms(letters.length * delay)),
-    disappearAt: timeAfter(start, ms(Infinity)),
-    obj: rect,
+    disappearAt: timeAfter(start, ms(letters.length * delay + 1000)),
+    obj: lastCursor,
   });
 
   const startText = 'スペースキーを押してスタート';
   const startTextWidth = ctx.measureText(startText).width;
   const startTextBlock: TextBlock = {
     tag: 'text-block',
-    position: {
+    fillColor: 'background',
+    position: fixed({
       x: canvasWidth / 2 - startTextWidth / 2,
       y: (canvasHeight * 3) / 4,
-    },
+    }),
     text: startText,
   };
+
+  const startTextRect: Rectangle = {
+    tag: 'rectangle',
+    width: {
+      from: 4,
+      to: startTextWidth,
+      easing: linear(
+        timeAfter(start, ms(letters.length * delay + 1000)),
+        ms(delay),
+      ),
+    },
+    height: fixed(60),
+    fillColor: 'primary',
+    position: {
+      from: { x: x + widthLast, y: prevPos.y },
+      to: {
+        x: canvasWidth / 2 - startTextWidth / 2,
+        y: (canvasHeight * 3) / 4 - 48,
+      },
+      easing: linear(
+        timeAfter(start, ms(letters.length * delay + 1000)),
+        ms(delay),
+      ),
+    },
+  };
   elements.push({
-    appearAt: timeAfter(start, ms(letters.length * delay + delay)),
+    appearAt: timeAfter(start, ms(letters.length * delay + 1000)),
+    disappearAt: timeAfter(start, ms(Infinity)),
+    obj: startTextRect,
+  });
+  elements.push({
+    appearAt: timeAfter(start, ms(letters.length * delay + 1000)),
     disappearAt: timeAfter(start, ms(Infinity)),
     obj: startTextBlock,
   });
 
   return { elements };
+};
+
+const renderGameObject = (
+  ctx: CanvasRenderingContext2D,
+  theme: Theme,
+  obj: GameObject,
+  now: TimeMs,
+) => {
+  if (obj.strokeColor) {
+    ctx.strokeStyle = theme[obj.strokeColor];
+  }
+  if (obj.fillColor) {
+    ctx.fillStyle = theme[obj.fillColor];
+  }
+
+  if (obj.tag === 'rectangle') {
+    renderRectangle(ctx, obj, now);
+  } else if (obj.tag === 'text-block') {
+    renderTextBlock(ctx, obj, now);
+  }
+};
+
+const renderRectangle = (
+  ctx: CanvasRenderingContext2D,
+  rect: Rectangle,
+  now: TimeMs,
+) => {
+  const { x, y } = animatePoint(rect.position, now);
+  const width = animateNumber(rect.width, now);
+  const height = animateNumber(rect.height, now);
+  ctx.fillRect(x, y, width, height);
+};
+
+const renderTextBlock = (
+  ctx: CanvasRenderingContext2D,
+  textBlock: TextBlock,
+  now: TimeMs,
+) => {
+  if (textBlock.font) {
+    ctx.font = textBlock.font;
+  }
+  if (textBlock.align) {
+    ctx.textAlign = textBlock.align;
+  } else {
+    ctx.textAlign = 'start';
+  }
+  const { x, y } = animatePoint(textBlock.position, now);
+  ctx.fillText(textBlock.text, x, y);
 };
 
 export type Renderer = {
@@ -140,12 +229,13 @@ export const getRenderer = (
   gameState: GameState,
 ) => {
   const ctx = canvas.getContext('2d')!;
-  const initialTime = timeNow();
+  //const initialTime = timeNow();
   const fpsCounter = getFpsCounter();
   let prevTime = timeNow();
   let tick = 0;
   let renderLoadingScreen: (() => void) | undefined;
   let renderTitleScreen: (() => void) | undefined;
+  let renderPlayScreen: (() => void) | undefined;
 
   const renderDebugInfo = (info: { label: string; value: string }[]) => {
     ctx.font = '12px monospace';
@@ -181,23 +271,19 @@ export const getRenderer = (
     return () => {
       ctx.font = '48px IBMPlexSans, IBMPlexSansJP';
       maintainTimeline(timeline, timeNow());
-      for (const element of listTimelineElements(timeline, timeNow())) {
-        if (element.tag === 'title-character') {
-          ctx.fillStyle = theme.foreground;
-          ctx.fillText(
-            element.character,
-            element.position.x,
-            element.position.y,
-          );
-        } else if (element.tag === 'rectangle') {
-          ctx.fillStyle = theme[element.color];
-          const { x, y } = animatePoint(element.position, timeNow());
-          ctx.fillRect(x, y, element.width, element.height);
-        } else if (element.tag === 'text-block') {
-          ctx.fillStyle = theme.foreground;
-          ctx.fillText(element.text, element.position.x, element.position.y);
-        }
+      for (const obj of listTimelineElements(timeline, timeNow())) {
+        renderGameObject(ctx, theme, obj, timeNow());
       }
+    };
+  };
+
+  const playScreenRenderer = () => {
+    return () => {
+      ctx.font = '24px IBMPlexSans, IBMPlexSansJP';
+      ctx.fillStyle = theme.foreground;
+      ctx.textAlign = 'center';
+      ctx.fillText('未実装', canvas.width / 2, canvas.height / 2);
+      ctx.textAlign = 'start';
     };
   };
 
@@ -213,18 +299,25 @@ export const getRenderer = (
 
     if (gameState.phase.tag === 'loading') {
       renderTitleScreen = undefined;
+      renderPlayScreen = undefined;
       if (!renderLoadingScreen) {
         renderLoadingScreen = loadingScreenRenderer();
       }
       renderLoadingScreen();
-    }
-
-    if (gameState.phase.tag === 'title') {
+    } else if (gameState.phase.tag === 'title') {
       renderLoadingScreen = undefined;
+      renderPlayScreen = undefined;
       if (!renderTitleScreen) {
         renderTitleScreen = titleScreenRenderer();
       }
       renderTitleScreen();
+    } else if (gameState.phase.tag === 'playing') {
+      renderLoadingScreen = undefined;
+      renderTitleScreen = undefined;
+      if (!renderPlayScreen) {
+        renderPlayScreen = playScreenRenderer();
+      }
+      renderPlayScreen();
     }
 
     fpsCounter.addSample();
