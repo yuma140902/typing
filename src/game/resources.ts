c776560205e66c059c108bea63e4cce31f65f7d1
yuma14 <yuma140902@gmail.com>
2024-06-02 13:43:50 +0900

    fix: resources

--- src/game/resources.ts
+++ src/game/resources.ts
@@ -1,16 +1,15 @@
 import { type ResourceLoader } from '../engine';
 
-export type GameResources = [FontFace, FontFace, unknown];
+export type GameResources = [FontFace, FontFace];
 
 export const resourceLoader: ResourceLoader<GameResources> = () => {
   let f1 = new FontFace('IBMPlexSans', 'url(/IBMPlexSans-Regular.woff2)');
   let f2 = new FontFace('IBMPlexSansJP', 'url(/IBMPlexSansJP-Regular.woff2)');
-  let r = new Promise((resolve) => setTimeout(() => resolve(42), 1000));
-  return Promise.all([f1.load(), f2.load(), r]);
+  return Promise.all([f1.load(), f2.load()]);
 };
 
 export const onResourceLoaded = (resource: GameResources) => {
-  const [f1, f2, _] = resource;
+  const [f1, f2] = resource;
   document.fonts.add(f1);
   document.fonts.add(f2);
 };
